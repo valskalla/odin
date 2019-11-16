e7ce76d79b3831c658df77dea1b21d8aae652a05
Sergey Kolbasov <sergey.kolbasov@zalando.fi>
2019-11-17 01:06:41 +0200

    Add writer spec

---
+++ core/src/test/scala/io/odin/writers/StdLogWriterSpec.scala
@@ -0,0 +1,22 @@
+package io.odin.writers
+
+import java.io.{ByteArrayOutputStream, PrintStream}
+
+import cats.effect.IO
+import io.odin.formatter.Formatter
+import io.odin.{LoggerMessage, OdinSpec}
+
+class StdLogWriterSpec extends OdinSpec {
+
+  it should "write formatted log to PrintStream" in {
+    forAll { loggerMessage: LoggerMessage =>
+      val baos = new ByteArrayOutputStream()
+      val ps = new PrintStream(baos)
+      StdLogWriter.mk[IO](ps).write(loggerMessage, Formatter.simple).unsafeRunSync()
+      val str = new String(baos.toByteArray)
+      str shouldBe Formatter.simple.format(loggerMessage) + "\n"
+      ps.close()
+    }
+  }
+
+}
