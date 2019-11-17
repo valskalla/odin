47904d8b1ef7d735fa49d6290fab1799c4a627f0
Sergey Kolbasov <sergey.kolbasov@zalando.fi>
2019-11-17 23:03:30 +0200

    Add RouterLogger spec

---
+++ core/src/test/scala/io/odin/loggers/RouterLoggerSpec.scala
@@ -0,0 +1,89 @@
+package io.odin.loggers
+
+import cats.data.WriterT
+import cats.effect.{IO, Timer}
+import cats.instances.list._
+import io.odin.{Level, Logger, LoggerMessage, OdinSpec}
+
+class RouterLoggerSpec extends OdinSpec {
+
+  implicit val timer: Timer[IO] = IO.timer(scala.concurrent.ExecutionContext.global)
+
+  type F[A] = WriterT[IO, List[(String, LoggerMessage)], A]
+
+  case class TestLogger(loggerName: String) extends DefaultLogger[F] {
+    def log(msg: LoggerMessage): F[Unit] = WriterT.tell(List(loggerName -> msg))
+  }
+
+  it should "route logs to corresponding loggers" in {
+    forAll { (p1: (String, LoggerMessage), p2: (String, LoggerMessage)) =>
+      val routerLogger = RouterLogger[F] {
+        case msg if msg == p1._2 => TestLogger(p1._1)
+        case msg if msg == p2._2 => TestLogger(p2._1)
+      }
+
+      val List(written1) = routerLogger.log(p1._2).written.unsafeRunSync()
+      val List(written2) = routerLogger.log(p2._2).written.unsafeRunSync()
+
+      written1 shouldBe p1
+      written2 shouldBe p2
+    }
+  }
+
+  it should "route based on the package" in {
+    forAll { (p1: (String, LoggerMessage), p2: (String, LoggerMessage)) =>
+      val routerLogger = RouterLogger.packageRoutingLogger[F](
+        p1._2.position.packageName -> TestLogger(p1._1),
+        p2._2.position.packageName -> TestLogger(p2._1)
+      )
+
+      val List(written1) = routerLogger.log(p1._2).written.unsafeRunSync()
+      val List(written2) = routerLogger.log(p2._2).written.unsafeRunSync()
+
+      written1 shouldBe p1
+      written2 shouldBe p2
+    }
+  }
+
+  it should "route based on the class" in {
+    forAll { (msg: String, loggerName1: String, loggerName2: String) =>
+      val routerLogger = RouterLogger.classRoutingLogger[F](
+        classOf[RouterLoggerSpec] -> TestLogger(loggerName1),
+        classOf[TestClass[F]] -> TestLogger(loggerName2)
+      )
+
+      val List((ln1, _)) = routerLogger.info(msg).written.unsafeRunSync()
+      val List((ln2, _)) = (new TestClass[F](routerLogger)).log(msg).written.unsafeRunSync()
+
+      ln1 shouldBe loggerName1
+      ln2 shouldBe loggerName2
+
+    }
+  }
+
+  it should "noop logs with level less than set" in {
+
+    type FF[A] = WriterT[IO, List[LoggerMessage], A]
+
+    val logger = new DefaultLogger[FF] {
+      def log(msg: LoggerMessage): FF[Unit] = WriterT.tell(List(msg))
+    }
+
+    forAll { (level: Level, msg: LoggerMessage) =>
+      val log = RouterLogger.withMinimalLevel[FF](level)(logger)
+      val written = log.log(msg).written.unsafeRunSync()
+      if (msg.level.value >= level.value) {
+        written shouldBe List(msg)
+      } else {
+        written shouldBe Nil
+      }
+    }
+  }
+
+}
+
+class TestClass[F[_]](logger: Logger[F]) {
+
+  def log(msg: String): F[Unit] = logger.info(msg)
+
+}
