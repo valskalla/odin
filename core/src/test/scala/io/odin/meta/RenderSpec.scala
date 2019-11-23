18eb17f6a440b6cc84d731622bcbf69b7fe6806e
Sergey Kolbasov <whoisliar@gmail.com>
2019-11-23 12:34:15 +0200

    Add Renderer specs (#11)

---
+++ core/src/test/scala/io/odin/meta/RenderSpec.scala
@@ -0,0 +1,34 @@
+package io.odin.meta
+
+import cats.Show
+import io.odin.OdinSpec
+import org.scalacheck.{Arbitrary, Gen}
+import cats.syntax.all._
+
+class RenderSpec extends OdinSpec {
+
+  it should "derive Render instance from cats.Show" in {
+    val renderer = implicitly[Render[Foo]]
+    forAll { foo: Foo =>
+      renderer.render(foo) shouldBe foo.show
+    }
+  }
+
+  it should "use .toString" in {
+    val renderer = Render.fromToString[Foo]
+    forAll { foo: Foo =>
+      renderer.render(foo) shouldBe foo.toString
+    }
+  }
+
+}
+
+case class Foo(x: String)
+
+object Foo {
+
+  implicit val fooShow: Show[Foo] = foo => foo.x
+
+  implicit val arbitraryFoo: Arbitrary[Foo] = Arbitrary(Gen.alphaNumStr.map(Foo(_)))
+
+}
