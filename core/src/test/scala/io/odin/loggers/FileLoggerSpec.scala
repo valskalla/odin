19e79bd0bd0856847ab4970b67157703a3cb0f00
sergeykolbasov <whoisliar@gmail.com>
2019-11-29 01:20:03 +0200

    Test async file logger

--- core/src/test/scala/io/odin/loggers/FileLoggerSpec.scala
+++ core/src/test/scala/io/odin/loggers/FileLoggerSpec.scala
@@ -4,8 +4,10 @@ import java.nio.file.{Files, Path, Paths}
 import java.util.UUID
 
 import cats.effect.Resource
+import io.odin._
 import io.odin.formatter.Formatter
 import io.odin.{LoggerMessage, OdinSpec}
+import scala.concurrent.duration._
 import monix.eval.Task
 import monix.execution.schedulers.TestScheduler
 
@@ -39,6 +41,23 @@ class FileLoggerSpec extends OdinSpec {
         fileName = path.toString
         logger <- FileLogger[Task](fileName, formatter)
         _ <- Resource.liftF(logger.log(loggerMessage))
+      } yield {
+        new String(Files.readAllBytes(Paths.get(fileName))) shouldBe loggerMessage
+          .map(formatter.format)
+          .mkString(lineSeparator) + (if (loggerMessage.isEmpty) "" else lineSeparator)
+      }).use(Task(_))
+        .runSyncUnsafe()
+    }
+  }
+
+  it should "write in async mode" in {
+    forAll { (loggerMessage: List[LoggerMessage], formatter: Formatter) =>
+      (for {
+        path <- fileResource
+        fileName = path.toString
+        logger <- asyncFileLogger[Task](fileName, formatter)
+        _ <- Resource.liftF(logger.log(loggerMessage))
+        _ = scheduler.tick(2.seconds)
       } yield {
         new String(Files.readAllBytes(Paths.get(fileName))) shouldBe loggerMessage
           .map(formatter.format)
