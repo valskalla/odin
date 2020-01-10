d0790de6bc70b241a8f84578e319ab92c2540dd9
Maksim Ochenashko <maxochenashko@gmail.com>
2020-01-10 11:57:08 +0100

    Add render interpolator (#65)

--- core/src/main/scala/io/odin/meta/Render.scala
+++ core/src/main/scala/io/odin/meta/Render.scala
@@ -5,11 +5,24 @@ import cats.Show
 /**
   * Type class that defines how message of type `M` got rendered into String
   */
+@scala.annotation.implicitNotFound(
+  """
+A value of type ${M} you're trying to log is missing a renderer.
+Make sure you have an implicit Render[${M}] in scope, i.e. inside of companion object of ${M}.
+"""
+)
 trait Render[M] {
   def render(m: M): String
 }
 
 object Render extends LowPriorityRender {
+
+  def apply[A](implicit instance: Render[A]): Render[A] = instance
+
+  final case class Rendered(override val toString: String) extends AnyVal
+  object Rendered {
+    implicit def mat[A](a: A)(implicit r: Render[A]): Rendered = Rendered(r.render(a))
+  }
 
   /**
     * Construct [[Render]] using default `.toString` method
