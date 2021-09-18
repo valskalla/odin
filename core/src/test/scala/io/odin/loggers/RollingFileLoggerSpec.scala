fa93ecd30c83377cace48daff2b5cf4c5ab22798
sergeykolbasov <whoisliar@gmail.com>
2021-09-18 21:31:59 +0300

    Increase sleep timeout for interval

--- core/src/test/scala/io/odin/loggers/RollingFileLoggerSpec.scala
+++ core/src/test/scala/io/odin/loggers/RollingFileLoggerSpec.scala
@@ -134,7 +134,7 @@ class RollingFileLoggerSpec extends OdinSpec {
             minLevel = Level.Trace
           )
           _ <- Resource.eval(logger.log(lm1))
-          _ <- Resource.eval(IO.sleep(1200.millis))
+          _ <- Resource.eval(IO.sleep(1500.millis))
           _ <- Resource.eval(logger.log(lm2))
         } yield {
           val log1 :: log2 :: Nil = ListDirectory(path).filter(_.isFile).sortBy(_.getName)
